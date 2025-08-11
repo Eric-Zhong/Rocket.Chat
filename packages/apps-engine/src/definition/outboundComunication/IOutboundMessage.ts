8eda6e405c3ae2ac8cc919abdd6c5a2cff6e0760
Kevin  Aleman <kaleman960@gmail.com>
2025-08-11 15:00:56 -0600

    types

--- packages/apps-engine/src/definition/outboundComunication/IOutboundMessage.ts
+++ packages/apps-engine/src/definition/outboundComunication/IOutboundMessage.ts
@@ -49,4 +49,23 @@ export type TemplateParameter =
 			type: 'media';
 			link: string;
 			format: 'image' | 'document' | 'video';
+	  }
+	| {
+			type: 'document';
+			document: {
+				link: string;
+				filename: string;
+			};
+	  }
+	| {
+			type: 'video';
+			video: {
+				link: string;
+			};
+	  }
+	| {
+			type: 'image';
+			image: {
+				link: string;
+			};
 	  };
